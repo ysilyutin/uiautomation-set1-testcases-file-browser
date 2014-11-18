c3eb41778d29141fddbcf21c9ed7d00c5ee3ee90
Yegor Silyutin <silyutiny@gmail.com>
2014-11-18 15:51:32 -0800

    Create test_6.js
    Open JPG files. Need to add assertions for this in 2nd version.

---
+++ test_6.js
@@ -0,0 +1,44 @@
+#import "assertions.js"
+#import "lang-ext.js"
+#import "uiautomation-ext.js"
+#import "screen.js"
+#import "test.js"
+#import "image_assertion.js"
+
+test("<<< Test - Open JPG files >>>", function(target,app) {
+     
+     var window = app.mainWindow();
+     var target = UIATarget.localTarget();
+     var app = target.frontMostApp();
+     var i = 1;
+     
+     app.logElementTree();
+     
+     target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["ios_share_1"].tap();
+     target.frontMostApp().mainWindow().searchBars()[0].tap();
+     target.tap({x:199.00, y:46.00});     
+     
+     UIALogger.logMessage("<<< Choose extensions of file >>>");
+     target.delay(4);
+     target.frontMostApp().keyboard().typeString("jpg");
+     target.frontMostApp().keyboard().typeString("\n");
+
+     UIALogger.logMessage("<<< Opening files one by one >>>");
+     target.delay(4);
+     target.frontMostApp().mainWindow().tableViews()["Search results"].cells()[i].tap();
+     target.frontMostApp().navigationBar().leftButton().tap();
+     
+                UIALogger.logMessage("<<< Creating loop for cells()[i] >>>");
+                for (i = 1; i < 10; i++) { 
+                    UIALogger.logMessage("<<< Open "+i+" file >>>");
+                    target.delay(1);
+                    target.frontMostApp().mainWindow().tableViews()[0].cells()[i].tap();
+                    target.frontMostApp().navigationBar().leftButton().tap();
+                }
+     
+     target.frontMostApp().tabBar().buttons()["Files"].tap();
+     
+     UIALogger.logMessage("<<< Wait 15 seconds... >>>");
+     target.delay(15);
+     
+     });
